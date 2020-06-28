513b18fb9b94a0fd457e19617e12b0ca0106666e
Alex Frutkin <frutkinalexander@gmail.com>
2020-06-28 16:04:46 -0400

    added a framework

--- main.cpp
+++ main.cpp
@@ -2,3 +2,9 @@
 
 //This should hopefully work as an AI high low player.
 //Hopefully the player doesn't cheat
+
+int main() {
+    //Do introductions
+    //Guess number and check with user
+    //Output number of guesses needed to find
+}
